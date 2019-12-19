c7e1b278cfa91da60806b4a3e7e04bfa80b00c32
rohanmohapatra <rohanironcrest14@gmail.com>
2019-12-19 16:42:14 +0530

    Email at Backend now Supported

--- api-controller/__init__.py
+++ api-controller/__init__.py
@@ -1,11 +1,12 @@
 from app import app
 from views.templates_view import templates_view
 from views.data_view import data_view
-
+from views.email_view import email_view
 base_url = '/api'
 
 app.register_blueprint(templates_view, url_prefix=base_url+'/templates')
 app.register_blueprint(data_view, url_prefix=base_url+'/data')
+app.register_blueprint(email_view, url_prefix=base_url+'/email')
 
 @app.errorhandler(500)
 def server_error(e):
