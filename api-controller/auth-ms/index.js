e494ab2851c59ab9e7ffe28d80a860dfb52ff9ad
sankarshanarao <sankarshana.rao@gmail.com>
2019-12-26 19:00:33 +0530

    Added auth ms

---
+++ api-controller/auth-ms/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'docassist-passport-secret', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+
+if(!isProduction) {
+    app.use(errorHandler());
+}
+
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/docassist-authms', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('debug', true);
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+  
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
+  
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
